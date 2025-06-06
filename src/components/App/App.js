948704f5dbd8b52ee188b8e02dc70775f6b2677e
Azad Hüseyin Kılıç <73038601+economicseeker@users.noreply.github.com>
2025-06-06 17:08:24 +0300

    Task 2: Add searchResults state and dynamic track listing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
-const mockTracks = [
-  { id: 1, name: 'Lose Yourself', artist: 'Eminem', album: '8 Mile' },
-  { id: 2, name: 'Levitating', artist: 'Dua Lipa', album: 'Future Nostalgia' },
-  { id: 3, name: 'Blinding Lights', artist: 'The Weeknd', album: 'After Hours' },
-];
-
 function App() {
-  return (
-      <div className="App">
-        <h1>Jammming 🎧</h1>
-        <SearchBar />
-        <div className="App-playlist">
-          <SearchResults tracks={mockTracks} />
-          <Playlist tracks={mockTracks} />
+    const [searchResults, setSearchResults] = useState([
+        {
+            id: 1,
+            name: 'Lose Yourself',
+            artist: 'Eminem',
+            album: '8 Mile',
+        },
+        {
+            id: 2,
+            name: 'Levitating',
+            artist: 'Dua Lipa',
+            album: 'Future Nostalgia',
+        },
+        {
+            id: 3,
+            name: 'Blinding Lights',
+            artist: 'The Weeknd',
+            album: 'After Hours',
+        },
+    ]);
+
+    return (
+        <div className="App">
+            <h1>Jammming 🎧</h1>
+            <SearchBar />
+            <div className="App-playlist">
+                <SearchResults tracks={searchResults} />
+                <Playlist tracks={searchResults} />
+            </div>
         </div>
-      </div>
-  );
+    );
 }
 
 export default App;
